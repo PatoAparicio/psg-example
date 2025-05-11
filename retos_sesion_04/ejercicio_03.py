405d9618a277ff5400e757c3abad8f83e22c865e
PatoAparicio <patriciaparicio94@gmail.com>
2025-05-10 22:38:27 -0400

    Ejercicio_03 Reto 4

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,12 @@
+print("REGLAS DE LUDO")
+
+print("Objetivo: Cada jugador debe mover sus 4 fichas desde su base hasta la meta final en el centro del tablero.")
+print("De 2 a 4 jugadores.")
+
+print("1. Cada jugador lanza un dado en su turno.")
+print("2. Una ficha sale de la base, si el dado es 6.")
+print("3. Vuelve a lanzar el dado y la ficha avanza la cantidad de casillas que indique el dado. Si vuelve a salir 6 puede avanzar 6 casillas o sacar una nueva ficha de la base.")
+print("4. Si una ficha cae en una casilla ocupada por una ficha rival, la elimina y la devuelve a su base.")
+print("5. Cuando una ficha da la vuelta al tablero, entra en su carril de llegada.")
+print("6. Debe sacar el número exacto de casillas en el dado para llegar al centro.")
+print("7. El primer jugador en llevar sus 4 fichas a la meta gana.")
