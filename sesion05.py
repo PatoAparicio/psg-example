69085ef059e51af76272c0b216ab72caa3e956a3
PatoAparicio <patriciaparicio94@gmail.com>
2025-05-23 10:31:45 -0400

    Clase sesión 05

---
+++ sesion05.py
@@ -0,0 +1,15 @@
+# Operaciones más complejas
+minutos = 300
+tiempo_extra_segundos = 3600
+horas = (minutos + tiempo_extra_segundos / 60) / 60
+print (horas)
+
+print ("Operadores de comparación con int - float")
+entero = 10
+flotante = 10.0
+print (entero < flotante)
+print (entero > flotante)
+print (entero == flotante)
+print (entero <= flotante)
+print (entero >= flotante)
+print (entero != flotante)
