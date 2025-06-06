063f58f03a2b8419eaa940eec91cd541fc4445b1
PatoAparicio <patriciaparicio94@gmail.com>
2025-06-06 16:42:50 -0400

    Clase sesion 07

---
+++ sesion07.py
@@ -0,0 +1,65 @@
+simple = 'Mi cadena permite comillas "dobles" en una sola línea'
+doble  = "Mi cadena permite comillas 'simples' en una sola línea"
+triple_simple = '''Mi cadena
+permite contenido 
+en varias líneas y comillas "dobles" '''
+triple_doble = """Mi cadena
+permite contenido 
+en varias líneas y comillas 'simples' """
+print (simple)
+print (doble)
+print (triple_simple)
+print (triple_doble)
+simple = 'Mi cadena permite comillas "dobles" en una sola línea'
+doble  = "Mi cadena permite comillas 'simples' en una sola línea"
+triple_simple = '''Mi cadena
+permite contenido 
+en varias líneas y comillas "dobles" '''
+triple_doble = """Mi cadena
+permite contenido 
+en varias líneas y comillas 'simples' """
+print (simple)
+print (doble)
+print (triple_simple)
+print (triple_doble)
+simple = 'Mi cadena permite comillas "dobles" en una sola línea'
+doble  = "Mi cadena permite comillas 'simples' en una sola línea"
+triple_simple = '''Mi cadena
+permite contenido 
+en varias líneas y comillas "dobles" '''
+triple_doble = """Mi cadena
+permite contenido 
+en varias líneas y comillas 'simples' """
+print (simple)
+print (doble)
+print (triple_simple)
+print (triple_doble)
+##entero = int(input("Ingrese un valor entero: "))
+##print(entero, type(entero))
+
+##flotante = float(input("Ingrese un valor flotante: "))
+##print(flotante, type(flotante))
+
+##booleano = bool(input("Ingrese un valor booleano: "))
+##print(booleano, type(booleano))
+
+print ("Indexado positivo")
+fruta = "banana"
+print (fruta)
+print (fruta[0])
+print (fruta[5])
+print ("Indexado negativo")
+fruta = "banana"
+print (fruta)
+print (fruta[-1])
+print (fruta[-3])
+
+print ("Slicing")
+ciudad =  "LaPaz-Bolivia"
+print (ciudad)
+print ("Slicing con índices positivos")
+print (ciudad[0:6])
+print (ciudad[0:6:2])
+print ("Slicing con índices negativos")
+print (ciudad[-10:-2])
+print (ciudad[-10:-2:2])
