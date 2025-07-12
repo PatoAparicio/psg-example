331257656838cd73306928d50a1f703ba02cdcd7
PatoAparicio <patriciaparicio94@gmail.com>
2025-07-12 13:32:03 -0400

    Corrección ejercicio_06

--- retos_sesion_07/ejercicio_05.py
+++ retos_sesion_07/ejercicio_05.py
@@ -1,5 +1,5 @@
 print("Escribe un programa que reciba una cadena y retorna verdadero o falso si es palindrome sin importar los espacios, mayúsculas o minúsculas")
-palabra=str(input("Ingrese una cadena: "))
+palabra=input("Ingrese una cadena: ")
 palabra_limpia = palabra.replace(" ", "").lower()
 resultado=palabra_limpia==palabra_limpia[::-1]
 print("La palabra es palindroma: ", resultado)
