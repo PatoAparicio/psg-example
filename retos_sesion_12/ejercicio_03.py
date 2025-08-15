f5570ac46c0017c9af07d27d3750f9d0dd1ad900
PatoAparicio <patriciaparicio94@gmail.com>
2025-08-15 13:29:51 -0400

    Correccion retos sesion 12

--- retos_sesion_12/ejercicio_03.py
+++ retos_sesion_12/ejercicio_03.py
@@ -1,5 +1,9 @@
 print("Jhon y Jess coleccionan autos a escala 1:64.")
 autos_jhon = {'Ferrari', 'Lamborghini', 'Porsche', 'Bugatti', 'McLaren'}
 autos_jess = {'Ferrari', 'Lamborghini', 'Tesla', 'Ford', 'Chevrolet'}
-autos_comunes = autos_jhon.intersection(autos_jess)
-print("Autos que tienen en común:", autos_comunes)
+if autos_jhon & autos_jess:
+    print("Sí tienen autos en común")
+    autos_comunes = autos_jhon & autos_jess
+    print("Los autos en común son:", autos_comunes)
+else:
+    print("No tienen autos en común")
