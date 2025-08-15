5b6a79c3226a20689f8fad08235fa872bbecbfe4
PatoAparicio <patriciaparicio94@gmail.com>
2025-08-15 13:37:31 -0400

    Correccion retos sesion 12

--- retos_sesion_12/ejercicio_05.py
+++ retos_sesion_12/ejercicio_05.py
@@ -1,10 +1,15 @@
 print("Tienes una app para gestionar contactos, cada contacto tiene un nombre y un número de teléfono, si el contacto tiene un número de teléfono válido (11 dígitos incluyendo el código de país) y un nombre valido se guarda en la lista de contactos y muestra el mensaje Contacto guardado, caso contrario se muestra el mensaje de error Datos incorrectos")
-
+contactos = []
 telefono = input("Ingrese su teléfono: ")
 nombre = input("Ingrese su nombre: ")
 valida_telefono=len(telefono) == 11
-
-if valida_telefono and nombre:
+valida_nombre = bool(nombre)
+print("--------------------------------")
+if valida_telefono and valida_nombre:
+    contacto = {"nombre": nombre, "telefono": telefono}
+    contactos.append(contacto)
     print("Contacto guardado")
 else:        
     print("Datos incorrectos")
+
+print("Lista de contactos:", list(contactos))
